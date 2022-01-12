78e38cbe98e59a1a1e2879770152f211e1f7916b
표승현 <42665051+PyoSH@users.noreply.github.com>
2022-01-12 22:47:49 +0900

    Update exercise_00_sub.cpp

--- src/exercise_00_sub.cpp
+++ src/exercise_00_sub.cpp
@@ -10,103 +10,73 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include "/home/catkin_ws/devel/include/exercise_00/VidInfo.h"
-void after_msg(ros::NodeHandle nh1);
+
 void msgcallBack(const exercise_00::VidInfo VidInfo);
 void imageCallback(const sensor_msgs::ImageConstPtr &msg1);
 
-// namespace enc = sensor_msgs::image_encodings;
+
 using namespace cv;
 using namespace std;
 
-int w_,h_;
-double fps_;
+
+int a=0; // Global parameter that act as loop conut
+
 int fourcc = VideoWriter::fourcc('D','I','V','X');
-// VideoWriter outputvideo;
-// int fourcc = cv2.VideoWriter_fourcc(*'X264');
+
+
 int delay;
 exercise_00::VidInfo info_vid; // msg that bring cam & video form data
-Mat input_V; // Matrix that saved into avi file 
-
-// VideoWriter outputvideo("/home/output1.avi", fourcc, 30, Size(640,480));
-//VideoWriter outputvideo("/home/output.avi", fourcc, fps_, Size(w_,h_));
-
+VideoWriter outputvideo; // Declare Videowriter function as global parameter
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "exrc_subscriber");
-	ros::NodeHandle nh1;
-    
-    after_msg(nh1);
-    
+    ros::NodeHandle nh;
+        
     // delay = cvRound(1000/fps_);
 
-    VideoWriter outputvideo("/home/output2.mp4", fourcc, fps_, Size(w_, h_));
+    image_transport::ImageTransport it(nh);
+    image_transport::Subscriber img_sub = it.subscribe("/camera/exercise/image_raw",100,imageCallback); // imgmsg subscribe
     
-    cv::namedWindow("view");
-    cv::startWindowThread();
-
-    image_transport::ImageTransport it(nh1);
     
-
+    ros::Subscriber info_sub = nh.subscribe<exercise_00::VidInfo>("VidInfo",1,msgcallBack); // Video info topic subscribe
+    
+    cout<<"!!!!!!!"<<endl;
     while(ros::ok()){
         
             cout <<"vid opened..." <<endl;
-
-            
-            image_transport::Subscriber img_sub = it.subscribe("/camera/exercise/image_raw",100,imageCallback);
-            
-            outputvideo.write(input_V);
-
-            imshow("img",input_V);
-
-            cout << "recording....." << endl;
-
-            waitKey(1);
-            ros::Rate loop_rate(100);
-            ros::spin();
-            // loop_rate.sleep();
-
-        // else if(!outputvideo.isOpened()){
-        //     cout<<"ewwww"<<endl;
-        // }
-        
+            ros::Rate loop_rate(50);           
+            ros::spinOnce();
+            loop_rate.sleep();
     }  
     
-    outputvideo.release();
+    outputvideo.release(); // stop writing video file
     cout << "end /" <<endl;
 
-    cv::destroyWindow("view");
+    return 0; // shut down the program
 }
 
 void msgcallBack(const exercise_00::VidInfo VidInfo){
-    info_vid.w= VidInfo.w;
-    info_vid.h= VidInfo.h;
-    info_vid.fps= VidInfo.fps;
-
-}
-
-void after_msg(ros::NodeHandle nh1){
-    ros::Subscriber info_sub = nh1.subscribe<exercise_00::VidInfo>("VidInfo",1,msgcallBack);
+    info_vid.w= VidInfo.w; // copy msg data(width) to global parameter
+    info_vid.h= VidInfo.h; // copy msg data(height) to global parameter
+    info_vid.fps= VidInfo.fps; // copy msg data(fps) to global parameter
     
-    w_ = info_vid.w;
-    h_ = info_vid.h;
-    fps_ = info_vid.fps;
-
-    // fourcc = VideoWriter::fourcc('D', 'I', 'V', 'X');
-    // delay = cvRound(1000/fps_);
+    // Make sure that "outputvideo.open(which means videowrite)" to be declared only when the file is first executed. 
+    if (a == 0){  
+        outputvideo.open("/home/output.avi", fourcc, info_vid.fps, Size(info_vid.w,info_vid.h));  
+    }
+        cout<<"w"<<info_vid.w<<" h: "<<info_vid.h<<" fps: "<<info_vid.fps<<"fourcc: "<<fourcc<<endl;
+    a++; 
 }
 
 void imageCallback(const sensor_msgs::ImageConstPtr &msg1){
-    // if(!output_vid.isOpened()){
-    //     cout<<"ewwww"<<endl;
-    // }
+    if(!outputvideo.isOpened()){
+        cout<<"ewwww"<<endl;
+    }
     
-    input_V = cv_bridge::toCvCopy(msg1,"bgr8")->image.clone();       
+    Mat input = cv_bridge::toCvCopy(msg1,"bgr8")->image.clone(); // Make cv::Mat that save image data from pub node
 
+    outputvideo.write(input); // Write video file using input matrix
+
+    waitKey(1); // 
 }
-
-
-
-
-
-    
