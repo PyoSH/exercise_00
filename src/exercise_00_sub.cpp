934d1ddbcd07454ad3a23c8f16066cf79b8ee784
표승현 <42665051+PyoSH@users.noreply.github.com>
2022-01-12 14:20:47 +0900

    Update exercise_00_sub.cpp

--- src/exercise_00_sub.cpp
+++ src/exercise_00_sub.cpp
@@ -1,3 +1,5 @@
+// sub : transform (input Mat => .mp4 file) until node shutdown. 
+
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <stdio.h>
@@ -8,98 +10,100 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include "/home/catkin_ws/devel/include/exercise_00/VidInfo.h"
+void after_msg(ros::NodeHandle nh1);
+void msgcallBack(const exercise_00::VidInfo VidInfo);
+void imageCallback(const sensor_msgs::ImageConstPtr &msg1);
 
 // namespace enc = sensor_msgs::image_encodings;
 using namespace cv;
 using namespace std;
 
-// sub : transform (input Mat => .mp4 file) until node shutdown. 
-
-// int w_,h_;
-// double fps_;
+int w_,h_;
+double fps_;
 int fourcc = VideoWriter::fourcc('D','I','V','X');
+// VideoWriter outputvideo;
 // int fourcc = cv2.VideoWriter_fourcc(*'X264');
-// int delay;
+int delay;
+exercise_00::VidInfo info_vid; // msg that bring cam & video form data
+Mat input_V; // Matrix that saved into avi file 
 
-VideoWriter outputvideo("/home/output.avi", fourcc, 30, Size(640,480));
+// VideoWriter outputvideo("/home/output1.avi", fourcc, 30, Size(640,480));
 //VideoWriter outputvideo("/home/output.avi", fourcc, fps_, Size(w_,h_));
 
-
-// Mat input_V;
-// exercise_00::VidInfo info_vid;
-
-// void msgcallBack(const exercise_00::VidInfo VidInfo){
-//     info_vid.w= VidInfo.w;
-//     info_vid.h= VidInfo.h;
-//     info_vid.fps= VidInfo.fps;
-
-// }
-
-// void after_msg(ros::NodeHandle nh1){
-//     ros::Subscriber info_sub = nh1.subscribe<exercise_00::VidInfo>("VidInfo",1,msgcallBack);
-    
-//     fourcc = VideoWriter::fourcc('D', 'I', 'V', 'X');
-//     delay = cvRound(1000/fps_);
-// }
-
-
-void imageCallback(const sensor_msgs::ImageConstPtr &msg1){
-    if(!outputvideo.isOpened()){
-        cout<<"ewwww"<<endl;
-    }
-    else{
-        Mat input_V = cv_bridge::toCvCopy(msg1,"bgr8")->image.clone();    
-    
-    outputvideo.write(input_V);
-    imshow("img",input_V);
-
-    cout << "recording....." << endl;
-
-    waitKey(1);
-    }   
-    
-}
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "exrc_subscriber");
 	ros::NodeHandle nh1;
     
-    {
-    // cv::namedWindow("view");
-    // cv::startWindowThread();
-
-    // after_msg(nh1);
-    // ros::Subscriber info_sub = nh1.subscribe<exercise_00::VidInfo>("VidInfo",1,msgcallBack);
+    after_msg(nh1);
     
-    // w_ = info_vid.w;
-    // h_ = info_vid.h;
-    // fps_ = info_vid.fps;
-    
-    // fourcc = VideoWriter::fourcc('m','p','4','v');
     // delay = cvRound(1000/fps_);
 
-    // cv::VideoWriter outputvideo("/home/output.mp4", fourcc, fps_, Size(w_, h_));
-    }
+    VideoWriter outputvideo("/home/output2.mp4", fourcc, fps_, Size(w_, h_));
+    
+    cv::namedWindow("view");
+    cv::startWindowThread();
 
     image_transport::ImageTransport it(nh1);
-    image_transport::Subscriber img_sub = it.subscribe("/camera/exercise/image_raw",100,imageCallback);
+    
 
-    
-    
     while(ros::ok()){
-        ros::Rate loop_rate(100);
-        ros::spinOnce();
-        loop_rate.sleep();
+        
+            cout <<"vid opened..." <<endl;
+
+            
+            image_transport::Subscriber img_sub = it.subscribe("/camera/exercise/image_raw",100,imageCallback);
+            
+            outputvideo.write(input_V);
+
+            imshow("img",input_V);
+
+            cout << "recording....." << endl;
+
+            waitKey(1);
+            ros::Rate loop_rate(100);
+            ros::spin();
+            // loop_rate.sleep();
+
+        // else if(!outputvideo.isOpened()){
+        //     cout<<"ewwww"<<endl;
+        // }
+        
     }  
     
     outputvideo.release();
     cout << "end /" <<endl;
 
-    // cv::destroyWindow("view");
+    cv::destroyWindow("view");
 }
 
+void msgcallBack(const exercise_00::VidInfo VidInfo){
+    info_vid.w= VidInfo.w;
+    info_vid.h= VidInfo.h;
+    info_vid.fps= VidInfo.fps;
 
+}
+
+void after_msg(ros::NodeHandle nh1){
+    ros::Subscriber info_sub = nh1.subscribe<exercise_00::VidInfo>("VidInfo",1,msgcallBack);
+    
+    w_ = info_vid.w;
+    h_ = info_vid.h;
+    fps_ = info_vid.fps;
+
+    // fourcc = VideoWriter::fourcc('D', 'I', 'V', 'X');
+    // delay = cvRound(1000/fps_);
+}
+
+void imageCallback(const sensor_msgs::ImageConstPtr &msg1){
+    // if(!output_vid.isOpened()){
+    //     cout<<"ewwww"<<endl;
+    // }
+    
+    input_V = cv_bridge::toCvCopy(msg1,"bgr8")->image.clone();       
+
+}
 
 
 
